a0f2e24398629748463b9adeca908053f214460c
JiHye0214 <qkrwlgp1526@gmail.com>
2023-11-13 22:01:45 +0900

    git commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { RouterProvider } from "react-router-dom";
+
+import router from "./routes/route"; 
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
+
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
+}
+export default App;
+
+//huiuui
